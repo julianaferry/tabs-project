1f86bf17958fb5b31383c28aa1b2a97ded97a870
juliana ferreira <julianaferry@gmail.com>
2020-07-23 13:55:38 +0100

    trying vanilla js and css fix

--- tabs.js
+++ tabs.js
@@ -1,16 +1,29 @@
 
-const TabActive = (event) => {
-    let activeTabs = document.querySelectorAll('.tabs-header__txt');
-    
-    activeTabs.forEach((tab) => {
-      tab.className = tab.className.replace('active', '');
-    });
-    
-    // activate new tab and panel
-    event.target.parentElement.className += ' active';
-    document.getElementsByClassName(event.target.href.split('#')[1]).className += ' active';
-  }
+const tabActive = () => {
+    // get the title and the texts
+    let titleElm = document.querySelectorAll('.tabs-header__title');   
+    let NewActiveTab = document.querySelectorAll('.tabs-content__txt');
+ 
+//for loop throught the titles
+ for (const i = 0; i < titleElm; i++) {
+     //click event add
+    titleElm[i].addEventListener('click', function() {
+       for (const i = 0; i < titleElm; i++) {
+            NewActiveTab[i].className = 'tabs-content__txt';
+       }
+ 
+
+    document.getElementById(this.dataset.id).className = 'tabs-content__txt active';
+
+
+    for (const i = 0; i < titleElm; i++) {
+        titleElm[i].className = 'tabs-header__title active';
+   }
+    this.className = 'tabs-header__title active';
+})
+}
   
-  const element = document.getElementsByClassName('tabs-header__title');
+}
   
-  element.addEventListener('click', tabActive, false);
+tabActive();
+
