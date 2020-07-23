c928847ba579c08232efb16e07bee9a0aafbe460
juliana ferreira <julianaferry@gmail.com>
2020-07-23 11:56:07 +0100

    styles started

--- tabs.js
+++ tabs.js
@@ -1,1 +1,16 @@
-console.log('log from tabs.js');
+
+const TabActive = (event) => {
+    let activeTabs = document.querySelectorAll('.tabs-header__txt');
+    
+    activeTabs.forEach((tab) => {
+      tab.className = tab.className.replace('active', '');
+    });
+    
+    // activate new tab and panel
+    event.target.parentElement.className += ' active';
+    document.getElementsByClassName(event.target.href.split('#')[1]).className += ' active';
+  }
+  
+  const element = document.getElementsByClassName('tabs-header__title');
+  
+  element.addEventListener('click', tabActive, false);
